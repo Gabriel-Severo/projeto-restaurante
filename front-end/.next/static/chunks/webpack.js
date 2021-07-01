3e2d714733b7891e913aae3ddab1d86eb75af313
gabriel.severo <gabriel.severo@azi.com.br>
2021-07-01 13:47:59 -0400

    Adicionado listagem de produtos, feitailtragem de produtos e ordenaçao

--- front-end/.next/static/chunks/webpack.js
+++ front-end/.next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1611c0ead45679a59427"; }
+/******/ 		__webpack_require__.h = function() { return "2b3225ad292e91114c32"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
